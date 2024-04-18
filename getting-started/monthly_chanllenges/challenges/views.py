dc3183e7255186ae42069f20b21f652b2b42cd12
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-04-18 11:11:24 -0600

    refactor: update how to retrieve month challenges

--- getting-started/monthly_chanllenges/challenges/views.py
+++ getting-started/monthly_chanllenges/challenges/views.py
@@ -4,63 +4,19 @@ from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from .data import challenges
 
 def monthly_challenge(request: HttpRequest, month: str):
-    challenge_text = ""
-    if month == "january":
-        challenge_text = challenges['january']
-    elif month == "february":
-        challenge_text = challenges['february']
-    elif month == "march":
-        challenge_text = challenges['march']
-    elif month == "april":
-        challenge_text = challenges['april']
-    elif month == "may":
-        challenge_text = challenges['may']
-    elif month == "june":
-        challenge_text = challenges['june']
-    elif month == "july":
-        challenge_text = challenges['july']
-    elif month == "august":
-        challenge_text = challenges['august']
-    elif month == "september":
-        challenge_text = challenges['september']
-    elif month == "october":
-        challenge_text = challenges['october']
-    elif month == "november":
-        challenge_text = challenges['november']
-    elif month == "december":
-        challenge_text = challenges['december']
-    else:
+    try:
+        challenge_text = challenges[month]
+    except:
         return HttpResponseNotFound('Month not supported')
-    return HttpResponse(challenge_text)
+    return HttpResponse("%s: %s" % (month.capitalize(), challenge_text))
 
 
 def monthly_challenge_by_number_month(request: HttpRequest, month: int):
-    challenge_text = ""
-    if month == 1:
-        challenge_text = challenges['january']
-    elif month == 2:
-        challenge_text = challenges['february']
-    elif month == 3:
-        challenge_text = challenges['march']
-    elif month == 4:
-        challenge_text = challenges['april']
-    elif month == 5:
-        challenge_text = challenges['may']
-    elif month == 6:
-        challenge_text = challenges['june']
-    elif month == 7:
-        challenge_text = challenges['july']
-    elif month == 6:
-        challenge_text = challenges['august']
-    elif month == 9:
-        challenge_text = challenges['september']
-    elif month == 10:
-        challenge_text = challenges['october']
-    elif month == 11:
-        challenge_text = challenges['november']
-    elif month == 12:
-        challenge_text = challenges['december']
+    if month > 0 and month <= 12:
+        keys =list(challenges.keys())
+        month_name = keys[month - 1]
+        challenge_text = challenges.get(month_name)
     else:
         return HttpResponseNotFound('Month not supported')
-    return HttpResponse(challenge_text)
+    return HttpResponse("%s: %s" % (month_name.capitalize(), challenge_text))
 
