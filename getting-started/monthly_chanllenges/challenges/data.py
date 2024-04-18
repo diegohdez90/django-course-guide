71a4b73b3d1659cafef1c531efe065d3fab5531f
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-04-18 10:58:54 -0600

    refactor: add challenges as variable

---
+++ getting-started/monthly_chanllenges/challenges/data.py
@@ -0,0 +1,14 @@
+challenges = {
+    "january": "Run",
+    "february": "Read",
+    "march": "Walk",
+    "april": "Jump",
+    "may": "Read",
+    "june": "Run",
+    "july": "Walk",
+    "august": "Jump",
+    "september": "Run",
+    "october":  "Walk",
+    "november": "Jump",
+    "december": "Read",
+}
--- getting-started/monthly_chanllenges/challenges/views.py
+++ getting-started/monthly_chanllenges/challenges/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 
+from .data import challenges
+
 def monthly_challenge(request: HttpRequest, month: str):
     challenge_text = ""
     if month == "january":
-        challenge_text = "Run"
+        challenge_text = challenges['january']
     elif month == "february":
-        challenge_text = "Read"
+        challenge_text = challenges['february']
     elif month == "march":
-        challenge_text = "Walk"
+        challenge_text = challenges['march']
     elif month == "april":
-        challenge_text = "Jump"
+        challenge_text = challenges['april']
     elif month == "may":
-        challenge_text = "Read"
+        challenge_text = challenges['may']
     elif month == "june":
-        challenge_text = "Run"
+        challenge_text = challenges['june']
     elif month == "july":
-        challenge_text = "Walk"
+        challenge_text = challenges['july']
     elif month == "august":
-        challenge_text = "Jump"
+        challenge_text = challenges['august']
     elif month == "september":
-        challenge_text = "Run"
+        challenge_text = challenges['september']
     elif month == "october":
-        challenge_text = "Walk"
+        challenge_text = challenges['october']
     elif month == "november":
-        challenge_text = "Jump"
+        challenge_text = challenges['november']
     elif month == "december":
-        challenge_text = "Read"
+        challenge_text = challenges['december']
     else:
         return HttpResponseNotFound('Month not supported')
     return HttpResponse(challenge_text)
@@ -35,29 +37,29 @@ def monthly_challenge(request: HttpRequest, month: str):
 def monthly_challenge_by_number_month(request: HttpRequest, month: int):
     challenge_text = ""
     if month == 1:
-        challenge_text = "Run"
+        challenge_text = challenges['january']
     elif month == 2:
-        challenge_text = "Read"
+        challenge_text = challenges['february']
     elif month == 3:
-        challenge_text = "Walk"
+        challenge_text = challenges['march']
     elif month == 4:
-        challenge_text = "Jump"
+        challenge_text = challenges['april']
     elif month == 5:
-        challenge_text = "Read"
+        challenge_text = challenges['may']
     elif month == 6:
-        challenge_text = "Run"
+        challenge_text = challenges['june']
     elif month == 7:
-        challenge_text = "Walk"
+        challenge_text = challenges['july']
     elif month == 6:
-        challenge_text = "Jump"
+        challenge_text = challenges['august']
     elif month == 9:
-        challenge_text = "Run"
+        challenge_text = challenges['september']
     elif month == 10:
-        challenge_text = "Walk"
+        challenge_text = challenges['october']
     elif month == 11:
-        challenge_text = "Jump"
+        challenge_text = challenges['november']
     elif month == 12:
-        challenge_text = "Read"
+        challenge_text = challenges['december']
     else:
         return HttpResponseNotFound('Month not supported')
     return HttpResponse(challenge_text)
