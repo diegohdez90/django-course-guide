bb65d41becc0f77f53209b41cdfcbfcc0487ed8b
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-04-18 10:37:29 -0600

    feat: add month challenges

--- getting-started/monthly_chanllenges/challenges/views.py
+++ getting-started/monthly_chanllenges/challenges/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 
-def index(request: HttpRequest):
-    return HttpResponse('It works')
-
-
-def february(request: HttpRequest):
-    return HttpResponse('February')
+def monthly_challenge(request: HttpRequest, month: str):
+    challenge_text = ""
+    if month == "january":
+        challenge_text = "Run"
+    elif month == "february":
+        challenge_text = "Read"
+    elif month == "march":
+        challenge_text = "Walk"
+    elif month == "april":
+        challenge_text = "Jump"
+    elif month == "may":
+        challenge_text = "Read"
+    elif month == "june":
+        challenge_text = "Run"
+    elif month == "july":
+        challenge_text = "Walk"
+    elif month == "august":
+        challenge_text = "Jump"
+    elif month == "september":
+        challenge_text = "Run"
+    elif month == "october":
+        challenge_text = "Walk"
+    elif month == "november":
+        challenge_text = "Jump"
+    elif month == "december":
+        challenge_text = "Read"
+    else:
+        return HttpResponseNotFound('Month not supported')
+    return HttpResponse(challenge_text)
 
