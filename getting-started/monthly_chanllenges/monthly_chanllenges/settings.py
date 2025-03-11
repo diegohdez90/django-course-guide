28732ec7e027ddee871262bc9922a1aec0f1d317
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-11 11:30:49 -0600

    feat: add global static dir

--- getting-started/monthly_chanllenges/monthly_chanllenges/settings.py
+++ getting-started/monthly_chanllenges/monthly_chanllenges/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'challenges/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
